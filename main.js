9ccdc27397ab1176c791f2f3c13a4f9464981d0f
altair201 <wilsondavianpa1998@outlook.com>
2023-04-20 09:42:56 -0500

    calculadora_parte2

--- main.js
+++ main.js
@@ -4,31 +4,47 @@ const operando=document.getElementById("operando")
 const display=document.getElementById("display")
 const botones=document.querySelector('.container_btns')
 
+let save;
 botones.addEventListener('click',(e)=>{
+    
     if(e.target.id=='suma'){
         operando.textContent="+"
+        save=+num1.value
+        num1.value=''
     }
     if(e.target.id=='rest'){
         operando.textContent="-"
+        save=+num1.value
+        num1.value=''
     }
     if(e.target.id=='mul'){
         operando.textContent="x"
+        save=+num1.value
+        num1.value=''
     }
     if(e.target.id=='div'){
         operando.textContent="/"
+        save=+num1.value
+        num1.value=''
     }
+    
     if(e.target.id=='igual'){
         if(operando.textContent=="+"){
-            display.textContent=+num1.value + +num2.value
+            display.textContent=save + +num1.value
         }
         if(operando.textContent=="-"){
-            display.textContent=+num1.value - +num2.value
+            display.textContent=save - +num1.value
         }
         if(operando.textContent=="x"){
-            display.textContent=+num1.value * +num2.value
+            display.textContent=save * +num1.value
         }
         if(operando.textContent=="/"){
-            display.textContent=+num1.value / +num2.value
+            display.textContent=save / +num1.value
         }
+    }
+    if(e.target.id=='limpiar'){
+        display.textContent=''
+        operando.textContent=""
+        num1.value=""
     }
 })
